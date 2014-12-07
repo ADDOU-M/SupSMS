9b3723ec9b5156f63a0a097f4d54df31730bb6e5
arsene <mcarsene16@yahoo.fr>
2014-12-07 08:43:14 +0100

    arsene 07/12/2014 à 08H37

--- SupSMS/SupSMS-service/src/main/java/com/supinfo/supsms/generiques/interfaces/IServiceGenerique.java
+++ SupSMS/SupSMS-service/src/main/java/com/supinfo/supsms/generiques/interfaces/IServiceGenerique.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package com.supinfo.supsms.generiques.interfaces;
 
 import java.util.Collection;
@@ -16,19 +15,32 @@ import java.util.List;
  * @param <T>
  * @param <PK>
  */
-public interface IServiceGenerique <T, PK extends java.io.Serializable> {
+public interface IServiceGenerique<T, PK extends java.io.Serializable> {
+
     public T ajouter(T t);
+
     public T modifier(T t);
+
     public boolean supprimer(PK k);
+
     public boolean supprimer(T t);
+
     public void supprimer();
+
     public List<T> lister();
+
     public List<T> lister(int debut, int nombre, String tri, boolean asc);
+
     List<T> lister(int debut, int nombre, String tri, boolean asc, IFiltreForm filtre);
-    public T recuperer(PK k);  
+
+    public T recuperer(PK k);
+
     public List<T> lister(IFiltreForm q);
+
     public List<T> listerCriteria();
+
     public void supprimer(Collection<T> ts);
-    public void supprimer(Iterator<T> ts);    
-    
+
+    public void supprimer(Iterator<T> ts);
+
 }
